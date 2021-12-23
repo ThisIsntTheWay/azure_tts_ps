3de7e39ddb85a33c110a3779914be2f407fb3e04
The Man <ThisIsntTheWay@users.noreply.github.com>
2021-12-23 21:59:21 +0100

    Default to max codec quality level

--- transcribe_sample.bat
+++ transcribe_sample.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -command "[string](Get-Content .\sample.txt -encoding UTF8) | .\main.ps1 -apiKey .\secret\apiKey -voice 'Nanami' -codec 'audio' -codecQuality 10"
+powershell -command "[string](Get-Content .\sample.txt -encoding UTF8) | .\main.ps1 -apiKey .\secret\apiKey -voice 'Nanami' -codec 'audio' -codecQuality 'max'"
